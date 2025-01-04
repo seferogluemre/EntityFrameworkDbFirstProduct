c05eec5d9de7a653c829782f7a85a98361bb36e0
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-04 09:39:13 +0300

    ekleme işleminin yazılması ve alanları temizleme fonksiyonunun yazılması

---
+++ FrmProduct.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace EntityFrameworkDbFirstProduct
+{
+    partial class FrmProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxtProductName = new System.Windows.Forms.TextBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnList = new System.Windows.Forms.Button();
+            this.TxtProductId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtProductStock = new System.Windows.Forms.TextBox();
+            this.TxtProductPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CmbProductCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(437, 8);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(758, 515);
+            this.dataGridView1.TabIndex = 52;
+            // 
+            // TxtProductName
+            // 
+            this.TxtProductName.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtProductName.Location = new System.Drawing.Point(139, 58);
+            this.TxtProductName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtProductName.Name = "TxtProductName";
+            this.TxtProductName.Size = new System.Drawing.Size(275, 29);
+            this.TxtProductName.TabIndex = 51;
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(227)))), ((int)(((byte)(194)))));
+            this.BtnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSearch.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSearch.Location = new System.Drawing.Point(28, 476);
+            this.BtnSearch.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(386, 47);
+            this.BtnSearch.TabIndex = 50;
+            this.BtnSearch.Text = "Ara";
+            this.BtnSearch.UseVisualStyleBackColor = false;
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnUpdate.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdate.Location = new System.Drawing.Point(28, 424);
+            this.BtnUpdate.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(386, 47);
+            this.BtnUpdate.TabIndex = 49;
+            this.BtnUpdate.Text = "Güncelle";
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BtnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(110)))), ((int)(((byte)(42)))));
+            this.BtnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.Location = new System.Drawing.Point(28, 372);
+            this.BtnDelete.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(386, 47);
+            this.BtnDelete.TabIndex = 48;
+            this.BtnDelete.Text = "Sil";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(123)))), ((int)(((byte)(39)))));
+            this.BtnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAdd.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAdd.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnAdd.Location = new System.Drawing.Point(28, 320);
+            this.BtnAdd.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(386, 47);
+            this.BtnAdd.TabIndex = 47;
+            this.BtnAdd.Text = "Ekle";
+            this.BtnAdd.UseVisualStyleBackColor = false;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnList
+            // 
+            this.BtnList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(106)))), ((int)(((byte)(103)))));
+            this.BtnList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnList.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnList.ForeColor = System.Drawing.Color.FloralWhite;
+            this.BtnList.Location = new System.Drawing.Point(28, 268);
+            this.BtnList.Margin = new System.Windows.Forms.Padding(4, 2, 4, 3);
+            this.BtnList.Name = "BtnList";
+            this.BtnList.Size = new System.Drawing.Size(386, 47);
+            this.BtnList.TabIndex = 46;
+            this.BtnList.Text = "Listele";
+            this.BtnList.UseVisualStyleBackColor = false;
+            // 
+            // TxtProductId
+            // 
+            this.TxtProductId.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtProductId.Location = new System.Drawing.Point(139, 22);
+            this.TxtProductId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtProductId.Name = "TxtProductId";
+            this.TxtProductId.Size = new System.Drawing.Size(275, 29);
+            this.TxtProductId.TabIndex = 45;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 21);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Ürün Adı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Ürün Id:";
+            // 
+            // TxtProductStock
+            // 
+            this.TxtProductStock.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtProductStock.Location = new System.Drawing.Point(139, 133);
+            this.TxtProductStock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtProductStock.Name = "TxtProductStock";
+            this.TxtProductStock.Size = new System.Drawing.Size(275, 29);
+            this.TxtProductStock.TabIndex = 56;
+            // 
+            // TxtProductPrice
+            // 
+            this.TxtProductPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtProductPrice.Location = new System.Drawing.Point(139, 97);
+            this.TxtProductPrice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtProductPrice.Name = "TxtProductPrice";
+            this.TxtProductPrice.Size = new System.Drawing.Size(275, 29);
+            this.TxtProductPrice.TabIndex = 55;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 21);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "Ürün Stok:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 100);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 21);
+            this.label4.TabIndex = 53;
+            this.label4.Text = "Ürün Fiyat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 175);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 21);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Ürün Kategori:";
+            // 
+            // CmbProductCategory
+            // 
+            this.CmbProductCategory.FormattingEnabled = true;
+            this.CmbProductCategory.Location = new System.Drawing.Point(139, 175);
+            this.CmbProductCategory.Name = "CmbProductCategory";
+            this.CmbProductCategory.Size = new System.Drawing.Size(275, 29);
+            this.CmbProductCategory.TabIndex = 58;
+            // 
+            // FrmProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1208, 535);
+            this.Controls.Add(this.CmbProductCategory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtProductStock);
+            this.Controls.Add(this.TxtProductPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TxtProductName);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnList);
+            this.Controls.Add(this.TxtProductId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ürün İşlemleri";
+            this.Load += new System.EventHandler(this.FrmProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox TxtProductName;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnList;
+        private System.Windows.Forms.TextBox TxtProductId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtProductStock;
+        private System.Windows.Forms.TextBox TxtProductPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CmbProductCategory;
+    }
+}
---
+++ FrmProduct.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDbFirstProduct
+{
+    public partial class FrmProduct : Form
+    {
+        public FrmProduct()
+        {
+            InitializeComponent();
+        }
+        DbProductEntities database = new DbProductEntities();
+        Product TableProduct = new Product();
+
+        private void clearTextBoxes()
+        {
+            TxtProductId.Clear();
+            TxtProductName.Clear();
+            TxtProductPrice.Clear ();
+            TxtProductStock.Clear();
+            CmbProductCategory.Text = "";
+        }
+
+        private void ProductList()
+        {
+            dataGridView1.DataSource = database.Product.ToList();
+        }
+        private void AddProduct()
+        {
+            string productName = TxtProductName.Text.Trim();
+            int productStock = int.Parse(TxtProductStock.Text.Trim());
+            decimal productPrice = decimal.Parse(TxtProductPrice.Text.Trim());
+            int selectedCategory = int.Parse(CmbProductCategory.SelectedValue.ToString());
+            
+            if(productName.Length >= 2 && productPrice.ToString() != "" && productStock.ToString()!="") 
+            {
+                TableProduct.ProductName = productName.ToString();
+                TableProduct.ProductPrice = productPrice;
+                TableProduct.ProductStock = productStock;
+                TableProduct.CategoryId = selectedCategory;
+                database.Product.Add(TableProduct);
+                database.SaveChanges();
+                ProductList();
+                
+            }
+            else
+            {
+                MessageBox.Show("Lütfen boş alan bırakmayınız", "Alanları kontrol ediniz", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+
+
+
+
+        private void FrmProduct_Load(object sender, EventArgs e)
+        {
+            ProductList();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            AddProduct();
+        }
+    }
+}
